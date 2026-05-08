7e0f06c830ba74f795b054d4441ae78a3f86d562
Sherif Ashraf Farooq <sherifashraffarooq@Sherifs-MacBook-Pro.local>
2026-05-08 12:33:12 -0700

    feat: implement Core Data persistence logic for favorite leagues

--- Active/Model/DataSource/CoreDataManager.swift
+++ Active/Model/DataSource/CoreDataManager.swift
@@ -5,4 +5,43 @@
 //  Created by Sherif Ashraf Farooq  on 08/05/2026.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    private init() {}
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    func fetchFavorites() -> [League] {
+        let request: NSFetchRequest<League> = League.fetchRequest()
+        do {
+            return try context.fetch(request)
+        } catch {
+            return []
+        }
+    }
+    
+    func saveLeague(key: String, name: String, logo: String, sport: String) {
+            let league = League(context: context)
+            
+            league.league_name = name
+            league.league_logo = logo
+            league.sport_type = sport
+        
+        saveContext()
+    }
+    
+    func deleteLeague(league: League) {
+        context.delete(league)
+        saveContext()
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            try? context.save()
+        }
+    }
+}
