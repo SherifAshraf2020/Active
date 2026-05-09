7c4447946ede038e2edbf3e04b933d24d41cfcf1
Sherif Ashraf Farooq <sherifashraffarooq@Sherifs-MacBook-Pro.local>
2026-05-08 23:12:25 -0700

    Refactor Favorites: empty state UI handling

--- Active/Modules/Favroite/FavoritesViewController.swift
+++ Active/Modules/Favroite/FavoritesViewController.swift
@@ -12,10 +12,31 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     @IBOutlet weak var favoritesTableView: UITableView!
     
     var favoriteLeagues: [League] = []
-        var presenter: FavoritesPresenter!
+    var presenter: FavoritesPresenter!
 
+    // MARK: - Empty State UI
+        let noDataView: UIView = {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.isHidden = true
+            return view
+        }()
+
+        let messageLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "No Favorites yet!\nStart adding your favorite leagues."
+            label.numberOfLines = 0
+            label.textAlignment = .center
+            label.textColor = .systemGray
+            label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+            return label
+        }()
+    
         override func viewDidLoad() {
             super.viewDidLoad()
+            
+            setupEmptyStateUI()
             
             presenter = FavoritesPresenter(view: self)
             
@@ -33,16 +54,37 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
             super.viewWillAppear(animated)
             presenter.getFavorites()
         }
+    
+    
+    // MARK: - Setup Empty State
+        private func setupEmptyStateUI() {
+        view.addSubview(noDataView)
+        noDataView.addSubview(messageLabel)
+            
+        NSLayoutConstraint.activate([
+            noDataView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            noDataView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            noDataView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            noDataView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+                
+            messageLabel.topAnchor.constraint(equalTo: noDataView.topAnchor),
+            messageLabel.bottomAnchor.constraint(equalTo: noDataView.bottomAnchor),
+            messageLabel.leadingAnchor.constraint(equalTo: noDataView.leadingAnchor),
+            messageLabel.trailingAnchor.constraint(equalTo: noDataView.trailingAnchor)
+            ])
+        }
 
         func showFavoriteLeagues(_ leagues: [League]) {
             self.favoriteLeagues = leagues
             self.favoritesTableView.isHidden = false
+            self.noDataView.isHidden = true
             self.favoritesTableView.reloadData()
         }
 
         func showEmptyState() {
             self.favoriteLeagues = []
             self.favoritesTableView.isHidden = true
+            self.noDataView.isHidden = false
         }
 
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
