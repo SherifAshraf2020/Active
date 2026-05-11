477e2846a88550d93d6659e5aede05b159d6627a
Sherif Ashraf Farooq <sherifashraffarooq@Sherifs-MacBook-Pro.local>
2026-05-10 20:00:31 -0700

    feat: implement OnboardingPresenter with basic slide loading logic

---
+++ Active/Modules/Onboarding/OnboardingPresenter.swift
@@ -0,0 +1,26 @@
+//
+//  OnboardingPresenter.swift
+//  Active
+//
+//  Created by Sherif Ashraf Farooq  on 10/05/2026.
+//
+
+import Foundation
+
+protocol OnboardingViewProtocol: AnyObject {
+    func showSlides(_ slides: [OnboardingSlide])
+}
+
+class OnboardingPresenter {
+    
+    weak var view: OnboardingViewProtocol?
+    
+    init(view: OnboardingViewProtocol) {
+        self.view = view
+    }
+    
+    func viewDidLoad() {
+        let slides = OnboardingService.getOnboardingSlides()
+        view?.showSlides(slides)
+    }
+}
